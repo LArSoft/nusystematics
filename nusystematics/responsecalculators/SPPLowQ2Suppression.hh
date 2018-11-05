418bd646461ad4cc47bf6a613d1ed12621e5b588
Luke Pickering <luketpickering@googlemail.com>
2018-11-05 15:05:39 -0600

    Adds P. Stowell 1pi suppression.

--- nusystematics/responsecalculators/SPPLowQ2Suppression.hh
+++ nusystematics/responsecalculators/SPPLowQ2Suppression.hh
@@ -1,7 +1,7 @@
 #ifndef nusystematics_RESPONSE_CALCULATORS_SPPLOWQ2SUPPRESSION_HH_SEEN
 #define nusystematics_RESPONSE_CALCULATORS_SPPLOWQ2SUPPRESSION_HH_SEEN
 
-#include "nusystematics/utility/enumclas2int.hxx"
+#include "nusystematics/utility/enumclass2int.hh"
 #include "nusystematics/utility/simbUtility.hh"
 
 #include <cmath>
@@ -11,9 +11,9 @@ namespace nusyst {
 ///
 ///
 ///\note From Phys. Rev. D 91, 012005 (2015) Fig. 9
-inline double GetSPPLowQ2SuppressionWeight(int mode_simb, int is_CC,
-                                           double Q2_GeV,
-                                           double parameter_value = 1) {
+inline double GetMINOSSPPLowQ2SuppressionWeight(int mode_simb, int is_CC,
+                                                double Q2_GeV,
+                                                double parameter_value = 1) {
 
   static double const A = 1.01;
   static double const Q2_Max = 0.7;
@@ -21,7 +21,7 @@ inline double GetSPPLowQ2SuppressionWeight(int mode_simb, int is_CC,
   static double const central_value = 0;
   static double const uncertainty = 1;
 
-  if ((mode_simb != e2i(simb_mode_copy::kRES)) || !is_CC) {
+  if ((mode_simb != e2i(simb_mode_copy::kRes)) || !is_CC) {
     return 1;
   }
 
@@ -31,6 +31,34 @@ inline double GetSPPLowQ2SuppressionWeight(int mode_simb, int is_CC,
 
   return (central_value + parameter_value * uncertainty) *
          (A / (1 + exp(1 - (sqrt(Q2_GeV) / Q0))));
+}
+
+///
+///
+///\note See P. Stowells thesis or upcoming MINERvA pion tune publication.
+inline double GetMINERvASPPLowQ2SuppressionWeight(int mode_simb, int is_CC,
+                                                  double Q2_GeV,
+                                                  double parameter_value = 1) {
+
+  static double const Q2_Max = 0.7;
+  static double const Q2_t1 = 0;
+  static double const Q2_t2 = 0.35;
+  static double const R1 = 0.3;
+  static double const R2 = 0.6;
+
+  if ((mode_simb != e2i(simb_mode_copy::kRes)) || !is_CC) {
+    return 1;
+  }
+
+  if ((Q2_GeV > Q2_Max) || (Q2_GeV < 0)) {
+    return 1;
+  }
+
+  double RQ2 = (R2 * ((Q2_GeV - Q2_t1) * (Q2_GeV - Q2_Max)) /
+         ((Q2_t2 - Q2_t1) * (Q2_t2 - Q2_Max))) +
+        (((Q2_GeV - Q2_t1) * (Q2_GeV - Q2_t2)) /
+         ((Q2_Max - Q2_t1) * (Q2_Max - Q2_t2)));
+  return 1 - parameter_value * ((1 - R1) * pow((1 - RQ2), 2));
 }
 } // namespace nusyst
 
